fe46de655fcdd1c11f234874847be3a61b8462dc
Albert Anthony <albert.anthony1999@gmail.com>
2020-03-16 10:23:46 -0500

    footer is more responsive

--- src/pages/home/Home.component.jsx
+++ src/pages/home/Home.component.jsx
@@ -62,7 +62,7 @@ const Home = () => (
                         <p className="sass">Sass</p>
                         <p className="bootstrap">Bootstrap</p>
                         <p className="adobe">Adobe XD</p>
-                        <p className="react">React</p>
+                        <p className="react">React/React Hooks</p>
                         <p className="redux">Redux</p>
                         <p className="git">git</p>
                         <p className="github">github</p>
@@ -117,6 +117,39 @@ const Home = () => (
                 </div>
             </section>
         </main>
+
+        <footer>
+            <div className="footer-container">
+                <div className="footer-about-me">
+                    <h2>About ALBERT</h2>
+                    <p>Hello there! My name is Albert and I'm a Front End Developer.</p> 
+                    <p>I started my journey as a self-taught web developer during senior year of High School when I first discovered the biggest HTML, CSS, and JavaScript. Ever since I have fallen in love with web development and spend most of my days taking my craft to the next level.</p> 
+                    <p>Nowadays I tend to code all of my projects from scratch in order to deliver a unique experience to every company.</p> 
+                    <p>Like many other people, I enjoy spending my free time increasing my knowledge of Web Development and building new and challenging projects.</p> 
+                    <p>Some of my favorite things to do include:</p> 
+                </div>
+
+                <div className="contact">
+                    <h2>Say hello:</h2>
+                    <p><FontAwesome className="fa-map-marker" name="map-marker"/> Address: Memphis, TN 38122</p>
+                    <p><FontAwesome className="fa-envelope" name="envelope"/> Email: albert.anthony1999@gmail.com</p>
+                    <p><FontAwesome className="fa-phone" name="phone"/> Phone: (901) 265-2710</p>
+                </div>
+            </div>
+
+            <div className="footer-hobbies">
+                <p><FontAwesome className="fas fa-users" name="user-friends"/> Tutoring AP Computer Science students on programming</p>
+                <p><FontAwesome className="fas fa-code" name="code"/> Learning modern standards and best practices of the industry</p>
+                <p><FontAwesome className="fas fa-cogs" name="cogs"/> Volunteering as a Vex Robotics coach at my local former high school</p>
+            </div>
+
+            <div className="footer-links">
+                <FontAwesome className="fas fa-github" name="github" size="2x"/>
+                <FontAwesome className="fas fa-linkedin" name="linkedin" size="2x"/>
+            </div>
+
+            <div className="copyright">© 2018 Albert Valdes</div>
+        </footer>
     </React.Fragment>
 );
 
